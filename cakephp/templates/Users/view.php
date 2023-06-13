4588fa6b2a8428ea5a8a8d8d7a52ecc9a95e9ee5
Wesley Au <wesley.au.2020@scis.smu.edu.sg>
2023-06-13 16:53:28 +0800

    Update view.php

--- cakephp/templates/Users/view.php
+++ cakephp/templates/Users/view.php
@@ -5,34 +5,35 @@
  * @var \App\Model\Entity\User $user
  */
 
- /** @var string input_year */
- $input_year = $_POST["input_year"]??date('Y');
+/** @var string input_year */
+$input_year = $_POST["input_year"]??date('Y');
 ?>
 
-<!-- Nav Bar -->
 <?php
-  $this->assign('title', __('User'));
-  $this->Breadcrumbs->add([
-      ['title' => 'Home', 'url' => '/'],
-      ['title' => 'List Users', 'url' => ['action' => 'index']],
-      ['title' => 'View'],
-  ]);
+$this->assign('title', __('User Details'));
+$this->Breadcrumbs->add([
+    ['title' => 'Home', 'url' => '/'],
+    ['title' => 'List Users', 'url' => ['action' => 'index']],
+    ['title' => 'View'],
+]);
 ?>
 
-<!-- User Details -->
 <div class="view card card-primary card-outline">
   <div class="card-header d-sm-flex">
-    <h3 class="card-title" style="float: left;">User Details</h3>
   </div>
   <div class="card-body table-responsive p-0">
     <table class="table table-hover text-nowrap">
         <tr>
-            <th><?= __('ID') ?></th>
+            <th><?= __('Id') ?></th>
             <td><?= $this->Number->format($user->id) ?></td>
         </tr>
         <tr>
             <th><?= __('Username') ?></th>
             <td><?= h($user->username) ?></td>
+        </tr>
+        <tr>
+            <th><?= __('Start Date') ?></th>
+            <td><?= h($user->start_date) ?></td>
         </tr>
     </table>
   </div>
@@ -40,87 +41,68 @@
     <div class="">
     </div>
     <div class="ml-auto">
-      <?= $this->Html->link(__('Edit Details'), ['action' => 'edit', $user->id], ['class' => 'btn btn-secondary']) ?>
+      <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id], ['class' => 'btn btn-secondary']) ?>
+      <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
     </div>
   </div>
 </div>
 
-<!-- Leave Details (Year) -->
-<div style="margin-top: 40px" class="related related-leaveDetails view card" >
-  <div class="card-header d-sm-flex" >
-    <h3 class="card-title">Leave Details (Year)</h3>
-  </div>
 
-    <!-- Leave Days Given -->
-    <div class="card-body table-responsive p-0">
-      <table style="margin-top: 20px" class="table table-hover text-nowrap">
-        <?php echo "<th>Leave Days Given ({$input_year})</th>" ?>
-        <tr>
-            <th><?= __('Carried Over') ?></th>
-            <th><?= __('Max Carry Over') ?></th>
-            <th><?= __('Annual Leave (AL)') ?></th>
-            <th><?= __('Medical Leave (ML)') ?></th>
-            <th><?= __('Hospital Leave (HL)') ?></th>
-        </tr>
-        <?php if (empty($user->leave_details)) { ?>
-          <tr>
-              <td colspan="12" class="text-muted">
-                Leave Details record not found!
-              </td>
-          </tr>
-        <?php } else{ ?>
-          <?php
-              foreach ($user->leave_details as $leaveDetails) {
-                if ($leaveDetails->year === $input_year) {
-                  if ($leaveDetails->leave_type_id === 1) {
-                    $AL_entitled = $leaveDetails->entitled;
-                    $AL_balance = $leaveDetails->balance;
-                  } elseif ($leaveDetails->leave_type_id === 2) {
-                    $ML_entitled = $leaveDetails->entitled;
-                    $ML_balance = $leaveDetails->balance;
-                  } elseif ($leaveDetails->leave_type_id === 3) {
-                    $HL_entitled = $leaveDetails->entitled;
-                    $HL_balance = $leaveDetails->balance;
-                  }
-                }
-              }
-          ?>
-
-          <td><?= h($leaveDetails->carried_over) ?></td>
-          <td><?= h($leaveDetails->max_carry_over) ?></td>
-          <td><?= h($AL_entitled) ?></td>
-          <td><?= h($ML_entitled) ?></td>
-          <td><?= h($HL_entitled) ?></td>
-        <?php } ?>
-      </table>
-
-      <!-- Leave Balance -->
-      <table style="margin-top: 20px" class="table table-hover text-nowrap">
-      <?php echo "<th>Leave Balance ({$input_year})</th>" ?>
-        <tr>
-            <th><?= __('Annual Leave (AL)') ?></th>
-            <th><?= __('Medical Leave (ML)') ?></th>
-            <th><?= __('Hospital Leave (HL)') ?></th>
-        </tr>
-        <?php if (empty($user->leave_details)) { ?>
-          <tr>
-              <td colspan="12" class="text-muted">
-                Leave Details record not found!
-              </td>
-          </tr>
-        <?php } else{ ?>
-          <tr>
-              <td><?= h($AL_balance) ?></td>
-              <td><?= h($ML_balance) ?></td>
-              <td><?= h($HL_balance) ?></td>
-          </tr>
-        <?php } ?>
-      </table>
+<div class="related related-leaveDetails view card">
+  <div class="card-header d-sm-flex">
+    <h3 class="card-title"><?= __('Related Leave Details') ?></h3>
+    <div class="card-toolbox">
+      <!-- <?= $this->Html->link(__('New'), ['controller' => 'LeaveDetails' , 'action' => 'add'], ['class' => 'btn btn-primary btn-sm']) ?>
+      <?= $this->Html->link(__('List '), ['controller' => 'LeaveDetails' , 'action' => 'index'], ['class' => 'btn btn-primary btn-sm']) ?> -->
     </div>
-
-    <div class="card-footer d-flex">
-      <div class="">
-      </div>
+  </div>
+  <div class="card-body table-responsive p-0">
+    <table class="table table-hover text-nowrap">
+      <tr>
+          <th><?= __('Id') ?></th>
+          <th><?= __('Leave Type') ?></th>
+          <th><?= __('Carried Over') ?></th>
+          <th><?= __('Max Carry Over') ?></th>
+          <th><?= __('Entitled') ?></th>
+          <th><?= __('Balance') ?></th>
+          <th><?= __('Earned') ?></th>
+          <th class="actions"><?= __('Actions') ?></th>
+      </tr>
+      <?php if (empty($user->leave_details)) { ?>
+        <tr>
+            <td colspan="10" class="text-muted">
+              Leave Details record not found!
+            </td>
+        </tr>
+      <?php }else{ ?>
+        <?php foreach ($user->leave_details as $leaveDetails) : ?>
+          <?php if ($leaveDetails->year === $input_year) : ?>
+            <?php
+              // get leave type name
+              
+            ?>
+          <tr>
+              <td><?= h($leaveDetails->id) ?></td>
+              <td><?= h($leaveDetails->leave_type_id) ?></td>
+              <td><?= h($leaveDetails->carried_over) ?></td>
+              <td><?= h($leaveDetails->max_carry_over) ?></td>
+              <td><?= h($leaveDetails->entitled) ?></td>
+              <td><?= h($leaveDetails->balance) ?></td>
+              <td><?= h($leaveDetails->earned) ?></td>
+              <td class="actions">
+                <?= $this->Html->link(__('View'), ['controller' => 'LeaveDetails', 'action' => 'view', $leaveDetails->id], ['class'=>'btn btn-xs btn-outline-primary']) ?>
+                <?= $this->Html->link(__('Edit'), ['controller' => 'LeaveDetails', 'action' => 'edit', $leaveDetails->id], ['class'=>'btn btn-xs btn-outline-primary']) ?>
+                <?= $this->Form->postLink(__('Delete'), ['controller' => 'LeaveDetails', 'action' => 'delete', $leaveDetails->id], ['class'=>'btn btn-xs btn-outline-danger', 'confirm' => __('Are you sure you want to delete # {0}?', $leaveDetails->id)]) ?>
+              </td>
+          </tr>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      <?php } ?>
+    </table>
+  </div>
+  <div class="card-footer d-flex">
+    <div class="">
+    </div>
     <div class="ml-auto">
         <!-- select year via dropdown menu -->
         <div style="float: left">
@@ -148,4 +130,3 @@
     </div>
   </div>
 </div>
-
