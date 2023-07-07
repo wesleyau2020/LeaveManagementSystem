87cb26f6f82f09ceb51aeb9e109d59cca9dc94de
Wesley Au <wesley.au.2020@scis.smu.edu.sg>
2023-07-07 10:47:57 +0800

    Creating leave-requests display.php

---
+++ cakephp/templates/LeaveRequests/display.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\LeaveRequest[]|\Cake\Collection\CollectionInterface $leaveRequests
+ */
+
+/** @var string inputYear */
+$inputYear = $_POST["inputYear"]??date('Y');
+?>
+
+<?php
+$this->assign('title', __('Pending requests'));
+$this->Breadcrumbs->add([
+    ['title' => 'Home', 'url' => '/'],
+    ['title' => 'List Leave Requests', 'url' => ['action' => 'index']],
+    ['title' => 'Display Requests'],
+]);
+?>
+
+<div class="card card-primary card-outline">
+  <div class="card-body table-responsive p-0">
+      <table class="table table-hover text-nowrap">
+          <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('ID') ?></th>
+                    <th><?= $this->Paginator->sort('name') ?></th>
+                    <th><?= $this->Paginator->sort('description') ?></th>
+                    <th><?= $this->Paginator->sort('days') ?></th>
+                    <th><?= $this->Paginator->sort('type') ?></th>
+                    <th><?= $this->Paginator->sort('leave_start') ?></th>
+                    <th><?= $this->Paginator->sort('leave_end') ?></th>
+                    <th><?= $this->Paginator->sort('status') ?></th>
+                    <th><?= $this->Paginator->sort('remark') ?></th>
+                </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($approvedLeaveRequests as $leaveRequest) : ?>
+                <tr>
+                  <td><?= $this->Number->format($leaveRequest->id) ?></td>
+                  <td><?= h($leaveRequest->user->username) ?></td>
+                  <td><?= h($leaveRequest->description) ?></td>
+                  <td><?= $this->Number->format($leaveRequest->days) ?></td>
+                  <td><?= h($leaveRequest->leave_type->name) ?></td>
+                  <td><?= h($leaveRequest->start_of_leave) ?></td>
+                  <td><?= h($leaveRequest->end_of_leave) ?></td>
+                  <td><?= h($leaveRequest->status) ?></td>
+                  <td><?= h($leaveRequest->remark) ?></td>
+                </tr>
+                <?php endforeach; ?>
+          </tbody>
+      </table>
+  </div>
+  <div class="card-footer d-flex">
+    <div class="ml-auto">
+      <div>
+        <!-- Toggle button to switch between active/inactive users -->
+        <?= $this->Form->create(null, ['url' => ['controller' => 'leaveRequests', 'action' => '/displayRejectedRequests']]); ?>
+        <?= $this->Html->link(__('Display Rejected Requests'), ['action' => 'displayRejectedRequests'], ['class' => 'btn btn-danger btn-sm']) ?>
+        <?= $this->Form->end(); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card card-primary card-outline">
+    <!-- <div class="card-header d-sm-flex"> -->
+        <!-- <h2 class="card-title"></h2> -->
+        <!-- <div class="card-toolbox"></div> -->
+    <!-- </div> -->
+    <!-- /.card-header -->
+    
+    <div class="card-body table-responsive p-0">
+        <table class="table table-hover text-nowrap">
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('name') ?></th>
+                    <th><?= $this->Paginator->sort('description') ?></th>
+                    <th><?= $this->Paginator->sort('days') ?></th>
+                    <th><?= $this->Paginator->sort('type') ?></th>
+                    <th><?= $this->Paginator->sort('leave_start') ?></th>
+                    <th><?= $this->Paginator->sort('leave_end') ?></th>
+                    <th><?= $this->Paginator->sort('status') ?></th>
+                    <th><?= $this->Paginator->sort('remark') ?></th>
+                    <th class="actions"><?= __('Approve') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($pendingLeaveRequests as $leaveRequest) : ?>
+                    <?php if ($leaveRequest->year === $inputYear): ?>
+                    <tr>
+                        <td><?= $this->Number->format($leaveRequest->id) ?></td>
+                        <td><?= h($leaveRequest->user->username) ?></td>
+                        <td><?= h($leaveRequest->description) ?></td>
+                        <td><?= $this->Number->format($leaveRequest->days) ?></td>
+                        <td><?= h($leaveRequest->leave_type->name) ?></td>
+                        <td><?= h($leaveRequest->start_of_leave) ?></td>
+                        <td><?= h($leaveRequest->end_of_leave) ?></td>
+                        <td><?= h($leaveRequest->status) ?></td>
+                        <td><?= h($leaveRequest->remark) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('Approve'), ['action' => 'approve', $leaveRequest->id], ['class' => 'btn btn-xs btn-outline-primary', 'escape' => false]) ?>
+                            <?= $this->Html->link(__('Reject'), ['action' => 'reject', $leaveRequest->id], ['class' => 'btn btn-xs btn-outline-danger', 'escape' => false]) ?>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- /.card-body -->
+</div>
